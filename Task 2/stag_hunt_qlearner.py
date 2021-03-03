b96cbd0d4997d0d38dbbcb0f074617c6ad79e5a8
Josse Huybrechts <josse.huybrechts@gmail.com>
2021-03-03 10:22:32 +0100

    Dynamics
    Added streamplot to stag hunt game, but does not work

--- Task 2/stag_hunt_qlearner.py
+++ Task 2/stag_hunt_qlearner.py
@@ -1,36 +1,46 @@
 import logging
-
 import pyspiel
+import matplotlib.pyplot as plt
 from open_spiel.python import rl_environment
 from open_spiel.python.algorithms import tabular_qlearner, random_agent
+from open_spiel.python.egt import dynamics
+from open_spiel.python.egt import utils
 from absl import flags, app
-import numpy as np
+from open_spiel.python.egt.visualization import Dynamics2x2Axes
 
 FLAGS = flags.FLAGS
 
 flags.DEFINE_integer("num_episodes", int(5e4), "Number of train episodes.")
 
+stag_hunt_game = pyspiel.create_matrix_game("stag_hunt", "Stag Hunt Game",
+                                            ["Stag", "Hare"], ["Stag", "Hare"],
+                                            [[4, 1], [3, 2]], [[4, 3], [1, 2]])
 
-def eval_against_random_bot(env, trained_agent, random_agent, num_episodes):
+
+def stag_hunt_dynamics():
+    payoff_tensor = utils.game_payoffs_array(stag_hunt_game)
+    stag_hunt_dynamic = dynamics.MultiPopulationDynamics(payoff_tensor, dynamics.replicator)
+    fig = plt.Figure()
+    Dynamics2x2Axes(fig, [0, 0, 4, 4]).streamplot(stag_hunt_dynamic)  # TODO: not working, does not show figure
+    plt.show()
+
+
+def eval_against_random_bot(env, q_agent, r_agent, num_episodes):
     """Evaluates `trained_agent` against `random_agent` for `num_episodes`."""
     score = 0
     for _ in range(num_episodes):
         time_step = env.reset()
-        trained_agent_output = trained_agent.step(time_step, is_evaluation=True)
-        random_agent_output = random_agent.step(time_step, is_evaluation=True)
-        time_step = env.step([trained_agent_output.action, random_agent_output.action])
+        q_agent_output = q_agent.step(time_step, is_evaluation=True)
+        r_agent_output = r_agent.step(time_step, is_evaluation=True)
+        time_step = env.step([q_agent_output.action, r_agent_output.action])
         score += time_step.rewards[0]
     return score / num_episodes
 
 
 # stag hunt game
 def main(_):
-    game = pyspiel.create_matrix_game("stag_hunt", "Stag Hunt Game",
-                                      ["Stag", "Hare"], ["Stag", "Hare"],
-                                      [[4, 1], [3, 2]], [[4, 3], [1, 2]])
-    num_players = 2
-
-    env = rl_environment.Environment(game)
+    stag_hunt_dynamics()
+    env = rl_environment.Environment(stag_hunt_game)
     num_actions = env.action_spec()["num_actions"]
 
     q_agent = tabular_qlearner.QLearner(player_id=0, num_actions=num_actions)
