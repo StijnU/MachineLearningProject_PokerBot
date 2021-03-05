f7a44c165782751b8b8199f15a4a8259d6196526
Josse Huybrechts <josse.huybrechts@gmail.com>
2021-03-05 15:15:03 +0100

    Play against qlearner
    Updated stag hunt game to learn against an other q learner

--- Task 2/stag_hunt_qlearner.py
+++ Task 2/stag_hunt_qlearner.py
@@ -6,8 +6,8 @@ from open_spiel.python.algorithms import tabular_qlearner, random_agent
 from open_spiel.python.egt import dynamics
 from open_spiel.python.egt import utils
 from absl import flags, app
-import numpy as np
-from open_spiel.python.egt.visualization import Dynamics2x2Axes, Dynamics3x3Axes
+from open_spiel.python.egt.visualization import Dynamics2x2Axes
+
 
 FLAGS = flags.FLAGS
 
@@ -22,46 +22,50 @@ def stag_hunt_dynamics():
     payoff_tensor = utils.game_payoffs_array(stag_hunt_game)
     stag_hunt_dynamic = dynamics.MultiPopulationDynamics(payoff_tensor, dynamics.replicator)
     ax = plt.subplot(projection="2x2")
-    # ax.quiver(stag_hunt_dynamic)
+    ax.quiver(stag_hunt_dynamic)
     ax.streamplot(stag_hunt_dynamic)
     plt.show()
 
 
-def eval_against_random_bot(env, q_agent, r_agent, num_episodes):
-    """Evaluates `trained_agent` against `random_agent` for `num_episodes`."""
+def eval_against_bot(env, q_agent, t_agent, num_episodes):
+    """Evaluates `trained_agent` against `test agent` for `num_episodes`."""
     score = 0
     for _ in range(num_episodes):
         time_step = env.reset()
         q_agent_output = q_agent.step(time_step, is_evaluation=True)
-        r_agent_output = r_agent.step(time_step, is_evaluation=True)
-        time_step = env.step([q_agent_output.action, r_agent_output.action])
+        t_agent_output = t_agent.step(time_step, is_evaluation=True)
+        time_step = env.step([q_agent_output.action, t_agent_output.action])
         score += time_step.rewards[0]
     return score / num_episodes
 
 
 # stag hunt game
+# learn 2 q-learning agents and evaluate the first against the second one (score is of the first agent)
 def main(_):
     stag_hunt_dynamics()
     env = rl_environment.Environment(stag_hunt_game)
     num_actions = env.action_spec()["num_actions"]
 
-    q_agent = tabular_qlearner.QLearner(player_id=0, num_actions=num_actions)
-
-    # random agents for evaluation
-    r_agent = random_agent.RandomAgent(player_id=1, num_actions=num_actions)
+    agents = [tabular_qlearner.QLearner(player_id=0, num_actions=num_actions), tabular_qlearner.QLearner(player_id=1, num_actions=num_actions)]
 
     # Train the agents
     training_episodes = FLAGS.num_episodes
     for cur_episode in range(training_episodes):
         if cur_episode % int(1e4) == 0:
-            win_rates = eval_against_random_bot(env, q_agent, r_agent, 1000)
+            win_rates = eval_against_bot(env, agents[0], agents[1], 1000)
             logging.info("Starting episode %s, average score %s", cur_episode, win_rates)
         time_step = env.reset()
-        q_agent_output = q_agent.step(time_step)
-        time_step = env.step([q_agent_output.action, r_agent.step(time_step).action])
+        actions = [0, 0]
+        for i in range(2):
+            agent_output = agents[i].step(time_step)
+            actions[i] = agent_output.action
+        time_step = env.step(actions)
 
         # Episode is over, step all agents with final info state.
-        q_agent.step(time_step)
+        for i in range(2):
+            agents[i].step(time_step)
+    time_step = env.reset()
+    print(agents[1].step(time_step, True).probs)
 
 
 if __name__ == "__main__":
