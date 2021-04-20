5117570c7f662488acadb5c627a1d6cd4c093d6e
Josse Huybrechts <josse.huybrechts@gmail.com>
2021-04-20 17:35:45 +0200

    fcpa agent, werkt niet

--- Task 4/fcpa_agent/fcpa_agent.py
+++ Task 4/fcpa_agent/fcpa_agent.py
@@ -14,8 +14,8 @@ import argparse
 import logging
 import numpy as np
 import pyspiel
-from open_spiel.python.algorithms import evaluate_bots
-
+from open_spiel.python.algorithms import evaluate_bots, rcfr
+import tensorflow.compat.v1 as tf
 
 logger = logging.getLogger('be.kuleuven.cs.dtai.fcpa')
 
@@ -42,15 +42,26 @@ class Agent(pyspiel.Bot):
         the tournament. Initializing the agent should thus take no more than
         a few seconds.
         """
+        self.policy = None
+        fcpa_game_string = pyspiel.hunl_game_string("fcpa")
+        game = pyspiel.load_game(fcpa_game_string)
+        self.model = rcfr.DeepRcfrModel(
+            game,
+            num_hidden_layers=1,
+            num_hidden_units=13,
+            num_hidden_factors=8,
+            use_skip_connections=True)
         pyspiel.Bot.__init__(self)
         self.player_id = player_id
+        self.state = None
+        self.previousAction = [None, None]
 
     def restart_at(self, state):
         """Starting a new game in the given state.
 
         :param state: The initial state of the game.
         """
-        pass
+        self.state = state
 
     def inform_action(self, state, player_id, action):
         """Let the bot know of the other agent's actions.
@@ -59,7 +70,8 @@ class Agent(pyspiel.Bot):
         :param player_id: The ID of the player that executed an action.
         :param action: The action which the player executed.
         """
-        pass
+        self.state = state
+        self.previousAction[player_id] = action
 
     def step(self, state):
         """Returns the selected action in the given state.
@@ -68,16 +80,75 @@ class Agent(pyspiel.Bot):
         :returns: The selected action from the legal actions, or
             `pyspiel.INVALID_ACTION` if there are no legal actions available.
         """
-        pass
+        if len(state.legal_actions()) == 0:
+            return pyspiel.INVALID_ACTION
+        features = rcfr.sequence_features(state, num_distinct_actions=state.num_distinct_actions())
+        action = self.model(features)
+        self.previousAction[self.player_id] = action
+        return action
+
+    def train(self):
+        """Trains the model and save the trained policy"""
+        buffer_size = -1
+        fcpa_game_string = pyspiel.hunl_game_string("fcpa")
+        game = pyspiel.load_game(fcpa_game_string)
+        models = [self.model, rcfr.DeepRcfrModel(
+            game,
+            num_hidden_layers=1,
+            num_hidden_units=13,
+            num_hidden_factors=8,
+            use_skip_connections=True)]
+        if buffer_size > 0:
+            solver = rcfr.ReservoirRcfrSolver(
+                game,
+                models,
+                buffer_size,
+                truncate_negative=False)
+        else:
+            solver = rcfr.RcfrSolver(
+                game,
+                models,
+                truncate_negative=False,
+                bootstrap=False)
+
+        def _train_fn(model, data):
+            """Train `model` on `data`."""
+            data = data.shuffle(100 * 10)
+            data = data.batch(100)
+            data = data.repeat(200)
+
+            optimizer = tf.keras.optimizers.Adam(lr=0.01, amsgrad=True)
+
+            @tf.function
+            def _train():
+                for x, y in data:
+                    optimizer.minimize(
+                        lambda: tf.losses.huber_loss(y, model(x), delta=0.01),  # pylint: disable=cell-var-from-loop
+                        model.trainable_variables)
+
+            _train()
+
+        # End of _train_fn
+
+        for i in range(100):
+            solver.evaluate_and_update_policy(_train_fn)
+            if i % 10 == 0:
+                conv = pyspiel.exploitability(game, solver.average_policy())
+                print("Iteration {} exploitability {}".format(i, conv))
+        self.policy = solver.current_policy()
+        print(self.policy)
 
 
 def test_api_calls():
     """This method calls a number of API calls that are required for the
     tournament. It should not trigger any Exceptions.
     """
+
     fcpa_game_string = pyspiel.hunl_game_string("fcpa")
     game = pyspiel.load_game(fcpa_game_string)
-    bots = [get_agent_for_tournament(player_id) for player_id in [0,1]]
+    bots = [get_agent_for_tournament(player_id) for player_id in [0, 1]]
+    for bot in bots:
+        bot.train()
     returns = evaluate_bots.evaluate_bots(game.new_initial_state(), bots, np.random)
     assert len(returns) == 2
     assert isinstance(returns[0], float)
@@ -91,4 +162,3 @@ def main(argv=None):
 
 if __name__ == "__main__":
     sys.exit(main())
-
