663d4f1cc155c79443e539c71053972846639da0
Josse Huybrechts <josse.huybrechts@gmail.com>
2021-04-25 09:54:24 +0200

    DeepCFR implemented

--- Task 4/fcpa_agent/fcpa_agent.py
+++ Task 4/fcpa_agent/fcpa_agent.py
@@ -18,7 +18,7 @@ import logging
 import numpy as np
 import pyspiel
 from open_spiel.python import rl_environment, policy
-from open_spiel.python.algorithms import evaluate_bots, nfsp
+from open_spiel.python.algorithms import evaluate_bots, deep_cfr
 import tensorflow.compat.v1 as tf
 from absl import app
 from absl import flags
@@ -41,45 +41,30 @@ def get_agent_for_tournament(player_id):
 
 def train():
     """Trains the model and save the trained policy"""
+    num_iterations = 100
+    num_traversals = 5
+
     fcpa_game_string = pyspiel.hunl_game_string("fcpa")
     game = pyspiel.load_game(fcpa_game_string)
-    num_players = 2
-    env_configs = {"players": num_players}
-    env = rl_environment.Environment(game, **env_configs)
-    info_state_size = env.observation_spec()["info_state"][0]
-    num_actions = env.action_spec()["num_actions"]
-
-    hidden_layers_sizes = [int(l) for l in [128, 32]]
-    kwargs = {
-        "replay_buffer_capacity": int(5e3),
-        "epsilon_decay_duration": int(3e3),
-        "epsilon_start": 0.06,
-        "epsilon_end": 0.001,
-    }
 
     with tf.Session() as sess:
-        agents = [
-            nfsp.NFSP(sess, idx, info_state_size, num_actions, hidden_layers_sizes,
-                      reservoir_buffer_capacity=int(5e3), anticipatory_param=0.1,
-                      **kwargs) for idx in range(num_players)
-        ]
-        learned_policy = NFSPPolicies(env, agents, nfsp.MODE.average_policy)
+        deep_cfr_solver = deep_cfr.DeepCFRSolver(
+            sess,
+            game,
+            policy_network_layers=(1024, 256),
+            advantage_network_layers=(1024, 256),
+            num_iterations=num_iterations,
+            num_traversals=num_traversals,
+            learning_rate=1e-3,
+            batch_size_advantage=128,
+            batch_size_strategy=1024,
+            memory_capacity=int(1e7),
+            policy_network_train_steps=400,
+            advantage_network_train_steps=20,
+            reinitialize_advantage_networks=False)
         sess.run(tf.global_variables_initializer())
-
-        for ep in range(int(3e3)):
-            time_step = env.reset()
-            while not time_step.last():
-                player_id = time_step.observations["current_player"]
-                agent_output = agents[player_id].step(time_step)
-                action_list = [agent_output.action]
-                time_step = env.step(action_list)
-
-            # Episode is over, step all agents with final info state.
-            for agent in agents:
-                agent.step(time_step)
-        # for agent in agents:
-        #     agent.save("/tmp/fcpa_checkpoints")
-    return learned_policy
+        deep_cfr_solver.solve()
+    return deep_cfr_solver
 
 
 class Agent(pyspiel.Bot):
@@ -94,37 +79,7 @@ class Agent(pyspiel.Bot):
         """
         pyspiel.Bot.__init__(self)
 
-        # # Reload policy
-        # fcpa_game_string = pyspiel.hunl_game_string("fcpa")
-        # game = pyspiel.load_game(fcpa_game_string)
-        # num_players = 2
-        # env_configs = {"players": num_players}
-        # env = rl_environment.Environment(game, **env_configs)
-        # info_state_size = env.observation_spec()["info_state"][0]
-        # num_actions = env.action_spec()["num_actions"]
-        #
-        # hidden_layers_sizes = [int(l) for l in [128, 32]]
-        # kwargs = {
-        #     "replay_buffer_capacity": int(5e3),
-        #     "epsilon_decay_duration": int(3e2),
-        #     "epsilon_start": 0.06,
-        #     "epsilon_end": 0.001,
-        # }
-        #
-        # with tf.Session() as sess:
-        #     agents = [
-        #         nfsp.NFSP(sess, idx, info_state_size, num_actions, hidden_layers_sizes,
-        #                   reservoir_buffer_capacity=int(5e3), anticipatory_param=0.1,
-        #                   **kwargs) for idx in range(num_players)
-        #     ]
-        #
-        #     sess.run(tf.global_variables_initializer())
-        #
-        #     for agent in agents:
-        #         print(agent.has_checkpoint("/tmp/fcpa_checkpoints"))
-        #         agent.restore("/tmp/fcpa_checkpoints")
-        #     learned_policy = NFSPPolicies(env, agents, nfsp.MODE.average_policy)
-        self.policy = None
+        self.solver = None
         self.player_id = player_id
         self.state = None
         self.previousAction = [None, None]
@@ -153,59 +108,34 @@ class Agent(pyspiel.Bot):
         :returns: The selected action from the legal actions, or
             `pyspiel.INVALID_ACTION` if there are no legal actions available.
         """
-        action_prob = self.policy.action_probabilities(state, self.player_id)
-        actions = list(action_prob.keys())
-        probs = list(action_prob.values())
-        action = np.random.choice(actions, p=probs)
-        return action
+        with tf.Session() as sess:
+            self.solver._session = sess
+            sess.run(tf.global_variables_initializer())
+
+            action_prob = self.solver.action_probabilities(state)
+            actions = list(action_prob.keys())
+            probs = list(action_prob.values())
+            sum_probs = sum(probs)
+            probs += (1 - sum_probs)/len(probs)
+            action = np.random.choice(actions, p=probs)
+            return action
 
 
 def test_api_calls():
     """This method calls a number of API calls that are required for the
     tournament. It should not trigger any Exceptions.
     """
-    learned_policy = train()
+    solver = train()
     fcpa_game_string = pyspiel.hunl_game_string("fcpa")
     game = pyspiel.load_game(fcpa_game_string)
     bots = [get_agent_for_tournament(player_id) for player_id in [0, 1]]
     for bot in bots:
-        bot.policy = learned_policy
+        bot.solver = solver
     returns = evaluate_bots.evaluate_bots(game.new_initial_state(), bots, np.random)
     assert len(returns) == 2
     assert isinstance(returns[0], float)
     assert isinstance(returns[1], float)
     print("SUCCESS!")
-
-
-class NFSPPolicies(policy.Policy):
-    """Joint policy to be evaluated."""
-
-    def __init__(self, env, nfsp_policies, mode):
-        game = env.game
-        player_ids = [0, 1]
-        super(NFSPPolicies, self).__init__(game, player_ids)
-        self._policies = nfsp_policies
-        self._mode = mode
-        self._obs = {"info_state": [None, None], "legal_actions": [None, None]}
-
-    def action_probabilities(self, state, player_id=None):
-        cur_player = state.current_player()
-        legal_actions = state.legal_actions(cur_player)
-
-        self._obs["current_player"] = cur_player
-        self._obs["info_state"][cur_player] = (
-            state.information_state_tensor(cur_player))
-        self._obs["legal_actions"][cur_player] = legal_actions
-
-        info_state = rl_environment.TimeStep(
-            observations=self._obs, rewards=None, discounts=None, step_type=None)
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            self._policies[cur_player]._session = sess
-            with self._policies[cur_player].temp_mode_as(self._mode):
-                p = self._policies[cur_player].step(info_state, is_evaluation=True).probs
-            prob_dict = {action: p[action] for action in legal_actions}
-        return prob_dict
 
 
 FLAGS = flags.FLAGS
@@ -222,9 +152,9 @@ def LoadAgent(agent_type, game, player_id, rng):
     if agent_type == "random":
         return uniform_random.UniformRandomBot(player_id, rng)
     elif agent_type == "agent":
-        learned_policy = train()
+        solver = train()
         bot = get_agent_for_tournament(player_id)
-        bot.policy = learned_policy
+        bot.solver = solver
         return bot
     elif agent_type == "check_call":
         policy = pyspiel.PreferredActionPolicy([1, 0])
@@ -258,12 +188,12 @@ def test_against_bots(_):
     num_rounds = 10
     utilities = [0, 0]
     # Play multiple rounds and take the average result
-    for _ in range(num_rounds):
+    for _ in range(2*num_rounds):
         state = game.new_initial_state()
 
-        # Print the initial state
-        print("INITIAL STATE")
-        print(str(state))
+        # # Print the initial state
+        # print("INITIAL STATE")
+        # print(str(state))
 
         while not state.is_terminal():
             # The state can be three different types: chance node,
@@ -273,27 +203,27 @@ def test_against_bots(_):
                 # Chance node: sample an outcome
                 outcomes = state.chance_outcomes()
                 num_actions = len(outcomes)
-                print("Chance node with " + str(num_actions) + " outcomes")
+                # print("Chance node with " + str(num_actions) + " outcomes")
                 action_list, prob_list = zip(*outcomes)
                 action = rng.choice(action_list, p=prob_list)
-                print("Sampled outcome: ",
-                      state.action_to_string(state.current_player(), action))
+                # print("Sampled outcome: ",
+                #       state.action_to_string(state.current_player(), action))
                 state.apply_action(action)
             else:
                 # Decision node: sample action for the single current player
                 legal_actions = state.legal_actions()
-                for action in legal_actions:
-                    print("Legal action: {} ({})".format(
-                        state.action_to_string(current_player, action), action))
+                # for action in legal_actions:
+                #     print("Legal action: {} ({})".format(
+                #         state.action_to_string(current_player, action), action))
                 action = agents[current_player].step(state)
                 action_string = state.action_to_string(current_player, action)
-                print("Player ", current_player, ", chose action: ",
-                      action_string)
+                # print("Player ", current_player, ", chose action: ",
+                #       action_string)
                 state.apply_action(action)
 
-            print("")
-            print("NEXT STATE:")
-            print(str(state))
+            # print("")
+            # print("NEXT STATE:")
+            # print(str(state))
 
         # Game is now done. Print utilities for each player
         returns = state.returns()
@@ -303,14 +233,14 @@ def test_against_bots(_):
         utilities.reverse()
         agents.reverse()
     print("---------------------------")
-    print("Average utility for player {} ({}) is {}".format(0, FLAGS.player0, utilities[0]/num_rounds))
-    print("Average utility for player {} ({}) is {}".format(1, FLAGS.player1, utilities[1]/num_rounds))
+    print("Average utility for player {} ({}) is {}".format(0, FLAGS.player0, utilities[0]/2/num_rounds))
+    print("Average utility for player {} ({}) is {}".format(1, FLAGS.player1, utilities[1]/2/num_rounds))
 
 
 def main(argv=None):
-    test_api_calls()
+    # test_api_calls()
     app.run(test_against_bots)
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    app.run(main)
