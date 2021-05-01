73eead55bb9d38e070b4af8712033d48678aaded
daanvw <daan.vanwesenbeeck@hotmail.com>
2021-05-01 12:19:36 +0200

    Last Changes Task 2

--- Task 2/dynamics_plots.py
+++ Task 2/dynamics_plots.py
@@ -15,25 +15,36 @@ def plot_dynamics(game, temp, kappa):
     replicator = dynamics.MultiPopulationDynamics(payoff_matrix, dynamics.replicator)
     # replicator = dynamics.SinglePopulationDynamics(payoff_matrix, dynamics.replicator)
     boltzmannq = dynamics.MultiPopulationDynamics(payoff_matrix, lambda state, fitness: dynamics.boltzmannq(state, fitness, temp))
-    # boltzmannq = dynamics.SinglePopulationDynamics(payoff_matrix, lambda state, fitness: dynamics.boltzmannq(state, fitness, temp))
     lenient = dynamics.MultiPopulationDynamics(
          payoff_matrix, lambda state, fitness: lenientboltzmannq(state, fitness, payoff_matrix[0], temp, kappa)
     )
     # lenient = dynamics.SinglePopulationDynamics(
     #     payoff_matrix, lambda state, fitness: lenientboltzmannq(state, fitness, payoff_matrix[0], temp, kappa)
     # )
-    _, (ax1, ax2, ax3) = plt.subplots(1, 3, subplot_kw=dict(projection="2x2"))
-    # _, (ax1, ax2, ax3) = plt.subplots(1, 3, subplot_kw=dict(projection="3x3"))
+    _, (ax1, ax2) = plt.subplots(1, 2, subplot_kw=dict(projection="2x2"))
+    # _, (ax1, ax2) = plt.subplots(1, 2, subplot_kw=dict(projection="3x3"))
     ax1.quiver(replicator)
-    ax1.set_title("replicator")
-    ax2.quiver(boltzmannq)
-    ax2.set_title("boltzmannq")
-    ax3.quiver(lenient)
-    ax3.set_title("lenient")
+    ax2.quiver(lenient)
     plt.tight_layout()
     plt.savefig('plot_dynamics.png')
     plt.show()
-    return ax1, ax2, ax3
+    return ax1, ax2
+
+def plot_LFAQ(game, temp, kappa):
+    payoff_matrix = utils.game_payoffs_array(game)
+    lenient = dynamics.MultiPopulationDynamics(
+         payoff_matrix, lambda state, fitness: lenientboltzmannq(state, fitness, payoff_matrix[0], temp, kappa)
+    )
+    # lenient = dynamics.SinglePopulationDynamics(
+    #     payoff_matrix, lambda state, fitness: lenientboltzmannq(state, fitness, payoff_matrix[0], temp, kappa)
+    # )
+    _, ax = plt.subplots(1, subplot_kw=dict(projection="2x2"))
+    # _, ax = plt.subplots(1, subplot_kw=dict(projection="3x3"))
+    ax.quiver(lenient)
+    plt.tight_layout()
+    plt.savefig('plot_lfaq.png')
+    plt.show()
+    return ax
 
 
 # Lenient Boltzmann Q: grafiek voor verschillende temperatures
@@ -51,7 +62,7 @@ def plot_temperatures(game, temperatures, kappa):
         # )
         axs[i].quiver(lenient)
     plt.tight_layout()
-    plt.savefig('plot_temperatures.png')
+    plt.savefig('plot_temperatures.png', bbox_inches="tight")
     plt.show()
 
 # Lenient Boltzmann Q: grafiek voor verschillende temperatures
@@ -69,31 +80,34 @@ def plot_kappas(game, temperature, kappas):
         # lenient = dynamics.SinglePopulationDynamics(
         #     payoff_matrix, lambda state, fitness: lenientboltzmannq(state, fitness, payoff_matrix[0], temperature, k)
         # )
+        axs[i].tick_params(labelsize=6)
         axs[i].quiver(lenient)
     plt.tight_layout()
-    plt.savefig('plot_kappas.png')
+    plt.savefig('plot_kappas.png', bbox_inches="tight")
     plt.show()  
 
 
 def main(_):
     game = pyspiel.load_game("matrix_mp")
-    # game = pyspiel.create_matrix_game("matching_pennies", "Matching Pennies",
-    #                                                ["Heads", "Tails"], ["Heads", "Tails"],
-    #                                                [[1, -1], [-1, 1]], [[-1, 1], [1, -1]])
-    # game = pyspiel.create_matrix_game("rps", "Rock Paper Scissors",
-    #                                 ["Rock", "Paper", "Scissors"], ["Rock", "Paper", "Scissors"],
-    #                                 [[0, -1, 1], [1, 0, -1], [-1, 1, 0]], [[0, 1, -1], [-1, 0, 1], [1, -1, 0]])
     # game = pyspiel.create_matrix_game("battle_of_sexes", "Battle of the Sexes",
     #                                                ["B", "S"], ["B", "S"],
     #                                               [[2, 0], [0, 1]], [[1, 0], [0, 2]])
+    # game = pyspiel.create_matrix_game("stag_hunt", "Stag Hunt Game",
+    #                                         ["Stag", "Hare"], ["Stag", "Hare"],
+    #                                         [[4, 1], [3, 3]], 
+    #                                         [[4, 3], [1, 3]])
+    # game = pyspiel.create_matrix_game("chicken_game", "Chicken Game",
+    #                                       ["Swerve", "Straight"], ["Swerve", "Straight"],
+    #                                       [[0, -1], [1, -10]], 
+    #                                       [[0, 1], [-1, -10]])
     kappa = 5
     kappas = [1, 2, 5, 25]
     temp = 0.1
-    temperatures = [float('inf'), 0.72877, 0.00001]
+    temperatures = [float('inf'), 0.72877, 0.001]
     plot_dynamics(game, temp, kappa)
+    plot_LFAQ(game, temp, kappa)
     plot_temperatures(game, temperatures, kappa)
     plot_kappas(game, temp, kappas)
-    # trajectories()
 
 def lenientboltzmannq(state, fitness, A, temp, kappa):
     y = np.linalg.pinv(A) @ fitness
@@ -101,39 +115,11 @@ def lenientboltzmannq(state, fitness, A, temp, kappa):
         y += np.full(y.shape, 1/len(y))
     n = len(A)
     u = np.zeros(n)
-    # print("FOUND y" + str(y))
+    # print("FOUND y " + str(y))
     for i in range(n):
         for j in range(n):
             u[i] += A[i, j] * y[j] * (sum(y[A[i, :] <= A[i, j]])**kappa - sum(y[A[i, :] < A[i, j]])**kappa) / sum(y[A[i, :] == A[i, j]])
     return dynamics.boltzmannq(state, u, temp)
-
-
-# def trajectories():
-#     game = pyspiel.load_game("matrix_mp")
-#     payoff_matrix = utils.game_payoffs_array(game)
-#     dyn = dynamics.MultiPopulationDynamics(
-#         payoff_matrix, lambda state, fitness: lenientboltzmannq(state, fitness, payoff_matrix[0], 0.1, 6)
-#     )
-#     start_list = np.array([[0.9,0.1,0.1,0.9],[0.75, 0.25, 0.25, 0.75], [0.6,0.4,0.4,0.6],[0.4,0.6,0.6,0.4],[0.25, 0.75, 0.75, 0.25],[0.1,0.9,0.9,0.1]])
-#
-#     for start in start_list:
-#         x = [start[0]]
-#         y = [start[2]]
-#         end = False
-#         while not end:
-#             temp = start + 0.03 * dyn(start)
-#             if np.allclose(temp, start):
-#                 end = True
-#             start = temp
-#             for i in range(len(start)):
-#                 start[i] = min(1, max(0, start[i]))
-#                 if start[i] == 0 or start[i] == 0:
-#                     end = True
-#             x.append(start[0])
-#             y.append(start[2])
-#         plt.plot(x, y, 'k')
-#     plt.show()
-
 
 if __name__ == "__main__":
     app.run(main)
