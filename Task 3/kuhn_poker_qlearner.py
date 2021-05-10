9843835302246665df7cbcb9681392559ee5543b
daanvw <daan.vanwesenbeeck@hotmail.com>
2021-05-10 15:58:25 +0200

    Error fix

--- Task 3/kuhn_poker_qlearner.py
+++ Task 3/kuhn_poker_qlearner.py
@@ -14,27 +14,6 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_integer("num_episodes", int(5e4), "Number of train episodes.")
 
-kuhn_poker_game = pyspiel.load_game('kuhn_poker')
-
-
-def eval_against_bot(env, q_agent, t_agent, num_episodes):
-    """Evaluates `trained_agent` against `test agent` for `num_episodes`."""
-    wins = np.zeros(2)
-    for player_pos in range(2):
-        if player_pos == 0:
-            cur_agents = [q_agent, t_agent]
-        else:
-            cur_agents = [t_agent, q_agent]
-        for _ in range(num_episodes):
-            time_step = env.reset()
-            while not time_step.last():
-                player_id = time_step.observations["current_player"]
-                print(time_step)
-                agent_output = cur_agents[player_id].step(time_step, is_evaluation=True)
-                time_step = env.step([agent_output.action])
-            if time_step.rewards[player_pos] > 0:
-                wins[player_pos] += 1
-    return wins / num_episodes
 
 
 # kuhn poker
@@ -49,9 +28,6 @@ def main(_):
     # Train the agents
     training_episodes = FLAGS.num_episodes
     for cur_episode in range(training_episodes):
-        if cur_episode % int(1e4) == 0:
-            win_rates = eval_against_bot(env, agents[0], agents[1], 1000)
-            logging.info("Starting episode %s, average score %s", cur_episode, win_rates)
         time_step = env.reset()
         while not time_step.last():
             player_id = time_step.observations["current_player"]
@@ -61,10 +37,10 @@ def main(_):
         # Episode is over, step all agents with final info state.
         for i in range(2):
             agents[i].step(time_step)
+
     # TODO: errort om een reden, check rps qlearner, ttt qlearner
     #for agent in agents:
        # time_step = env.reset()
-       # print(agent.step(time_step, True).probs)
 
 if __name__ == "__main__":
     app.run(main)
